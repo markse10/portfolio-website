ad0a7742cbe279efce9dbc101143fcbed00203a4
markse10 <101365531+markse10@users.noreply.github.com>
2022-04-18 20:43:27 -0700

    adding jquery and hamburger menu to mobile screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to my Portfolio!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
